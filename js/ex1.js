9a39d3d3e323522c03d3df88925774093c5019ba
Mathis VIEILLY <mathis.vieilly@epitech.eu>
2023-09-08 12:00:15 -0700

    Ex 1

--- js/ex1.js
+++ js/ex1.js
@@ -1,5 +1,16 @@
-/* Tutorial 4
-   Example 1 JavaScript code
-*/
+const input = prompt("Please enter a number between 1 and 100");
 
-console.log("Mathis' Output from Tutorial 4 Example 1");
+if (isNaN(input)) {
+  console.log(`Sorry, ${input} is not a number.`);
+} else {
+  const num = Number(input);
+  if (!Number.isInteger(Number(input))) {
+    console.log(`Sorry, ${input} is not an integer.`);
+  } else {
+    if (num > 0 && num <= 100) {
+      console.log(`Thank you! You entered ${num}, a valid number.`);
+    } else {
+      console.log(`Sorry, ${num} is not a valid entry.`);
+    }
+  }
+}
--- js/ex2.js
+++
@@ -1,5 +0,0 @@
-/* Tutorial 4
-   Example 2 JavaScript code
-*/
-
-console.log("Mathis' Output from Tutorial 4 Example 2");
